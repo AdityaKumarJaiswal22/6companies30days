35fa99def84dde7eaddd719acb54b6491d2e919b
Aditya Kumar Jaiswal <76932761+AdityaKumarJaiswal22@users.noreply.github.com>
2022-01-05 21:15:23 +0530

    Update and rename Problem2.ajava to Problem2.java

--- Goldman Sachs/Problem2.java
+++ Goldman Sachs/Problem2.java
@@ -12,6 +12,18 @@ Two rectangles sharing a side are considered overlapping.
 */
 
 
+class Problem2{
+    public static void main(String[] args){
+        int[][] points = new int[4][2];
+        points[0][0] = 0; points[0][1] = 10;
+        points[1][0] = 10;points[1][1] = 0;
+        points[2][0] = 5; points[2][1] = 5;
+        points[3][0] = 15;points[3][1] = 0;
+        Solution os = new Solution();
+        System.out.println(os.doOverlap(points[0], points[1], points[2], points[3])); // OP should be 1
+    }
+}
+
 class Solution {
     int doOverlap(int L1[], int R1[], int L2[], int R2[]) {
         if(L1[1] < R2[1] || R1[1] > L2[1]){
