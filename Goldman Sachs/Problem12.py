9b6f3d4d716552bf3c53ffc16143ee542e10bf34
Aditya Kumar Jaiswal <76932761+AdityaKumarJaiswal22@users.noreply.github.com>
2022-01-09 12:01:29 +0530

    Create Problem12.py

---
+++ Goldman Sachs/Problem12.py
@@ -0,0 +1,13 @@
+# Count of number of squares in NxN chessBoard
+
+def countSquares(n):
+
+    # n*(n+1)*(2n+1)/6
+    return ((n * (n + 1) / 2) * (2 * n + 1) / 3)
+
+
+# Driver code
+
+n = 4
+print("Count of squares is ",countSquares(n))
+
